02d6f6e11836577ab71138e1423abcd0e5d79219
TiberiuC39 <tiberiu.catrangiu@gmail.com>
2024-08-11 14:23:44 +0300

    Removed unused usings

--- Wind.xaml.cs
+++ Wind.xaml.cs
@@ -1,21 +1,11 @@
 ﻿using iRacingSdkWrapper;
-using iRacingSdkWrapper.Bitfields;
 using SharpOverlay.Models;
 using SharpOverlay.Services;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace SharpOverlay
 {
